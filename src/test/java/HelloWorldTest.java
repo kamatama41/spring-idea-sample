0fefade985bbb91a0027b1466bfe3e3fcc0563f7
kamatama41 <shiketaudonko41@gmail.com>
2013-03-10 18:57:28 +0900

    Add junit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * User: kamatama_41
+ */
+public class HelloWorldTest {
+	@Test
+	public void testHello() throws Exception {
+		Assert.fail("not implement.");
+	}
+}
